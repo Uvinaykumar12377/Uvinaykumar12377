8625521c5c4f2dda6a3353f7229074e84d1c1947
Uvinaykumar12377 <162982619+Uvinaykumar12377@users.noreply.github.com>
2024-03-11 16:15:35 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,61 @@
+'''
+controls and data structures --->ABC language
+strings and its foramts --->fron c
+Regural expressions ---> pearl 
+Exceoting handling ---> Module 3
+Magic methods from --->forton
+
+'''
+'''
+Python is a
+--> Object oriented programming language
+--> Dynamically type language
+--> Scripting language
+--> interpreted language
+--> Compiled language
+--> Functional Programming language
+
+'''
+Flavours of python
+--> Cpython - written in C,Most common implementation of python.
+--> Jython - java implementation of paython.
+--> IronPython - implementation of the paython programming language which is tightly integrated with.NET.
+--> Brython - Browser Python,rund in browsers
+--> Ruby python - Bridge between Python and Ruby Interpreters
+--> Micro Python - Runs in micro controllers
+
+'''
+
+# ---> print(object,sep,end,file,flush)
+#print has 5 parameters
+
+#object --> the value to be printed is object
+#sep--> seperator
+#Eg
+#a = 8
+#b = 9
+#c = 89
+#print(a,b,c, sep ='$')
+
+#end --> which is used to add the string at the end of the value,
+#Eg:
+#print(str(a),b,end='%%')
+#print(str(c))
+
+# ! --> variables
+# ?variable is a name which is used to identify the value of variable
+# ?which is stored in a specific memory location
+
+# ! Rules to define variables
+#1.] Variable have to start with only alphabets, underscores
+#2.) the char following the 1st char be alphabets, numbers, underscores
+#3.) np space are allowed
+#4.) no special characters like !@#$^&*()_+
+#5.) case sensitive
+#6.) keywords are not allowed as variables
+
+# ! ways to define variables
+
+# a=8
+# b=9
+
