8838de2439d1eb172c34b2f0a08347e976a7154f
Uvinaykumar12377 <162982619+Uvinaykumar12377@users.noreply.github.com>
2024-03-21 15:54:08 +0530

    Add files via upload

--- py.10..py
+++ py.10..py
@@ -104,6 +104,183 @@ obj.add(3,4)
 obj.ad(1,2,3)
 
 --->Abstraction
+The process of hiding the implimentatiom details is abstraction
+
+Eg:1
+
+from abc import ABC,abstractmethod
+class shapes(ABC):
+    @abstractmethod
+    def sides(self):
+        print("All shapes have sides except circle")
+
+class triangle:
+    def triangle_sides(self):
+        print("3 sides")
+        
+    def name(self):
+        print("Iam triangle")
+            
+    def sides(self):
+        pass
+
+class square:
+    def square(self):
+        print("4 sides")
+
+    def sides(self):
+        pass
+
+tr=triangle()
+tr.triangle_sides()
+tr.name()
+
+#! Rules to define abstract class1
+1.)Abstract class cannot be instantiated
+2.)from abc import ABC,abstractmethod
+3.)subclass the normal class with ABC
+4.)convert the normal method inside the abstract class to be abstract method
+5.)All the child classes have to be subclassed with abstract class
+6.)The abstract method should be present inthe child classes
+
+Eg:2
+super()--->used to access the parent class method from child class method
+from abc import ABC,abstractmethod
+class c1(ABC):
+    @abstractmethod
+    def m1(self):
+        print("This is abstract class")
+
+class c2(c1):
+    def m2(self):
+        super().m1()
+        print("Iam child 1")
+
+    def m1(self):
+        pass
+
+class2=c2()
+class2.m2()
+
+Eg:3
+from abc import ABC,abstractmethod
+class password(ABC):
+    @abstractmethod
+    def pwd(self):
+        pswd="sidd1994$"
+        return pswd
+
+class login(password):
+    def validate(self,name,passwrd):
+        if super().pwd()==passwrd:
+           print("welcome",name,'!!')
+           print("Login successfull")
+        else:
+            print("please check the password")
+            
+    def pwd(self):
+        pass
+    
+Login=login()
+name=input("Enter the name:")
+pwd=input("Enter the password:")
+Login.validate(name,pwd)
+
+#! Encapsulation
+Eg:1
+class car:
+    _name="BMW" private variable
+
+c1=car()
+print(c1.name)
+c1.name="Audi"
+print(c1.name)
+
+Eg:2
+accessing private date outside the class
+class c1:
+    _phone='9347161677'
+
+    def display(self):
+        print(self._phone)
+
+c=c1()
+c.display()
+
+Eg:3
+declare private method
+class class1:
+    def _m1(self): private method
+        print("Iam private method")
+
+    def_int_(self):
+        self._m1()
+c=class1()
+c._m1()  error
+
+nested class
+ class class1:
+     class class2:
+         name="sidhu"
+
+     def display(self):
+         print(self.name)
+ obj1=class2()
+
+obj=class1()
+obj.obj1.display()
+
+d1={"shirt":1000,"pant":1500,"shoes":900,"handkey":30}
+for val in d1:
+    if d1[val]==min(d1.values()):
+        print(val)
+    
+for val in d1:
+    if d1[val]==max(d1.values()):
+        print(val)
+         
+for val in d1:
+    if val.startswith('s') or val.startswith('s'):
+        print(val)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
